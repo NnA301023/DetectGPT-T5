c2781c8a83907bb0bb552cc2b93c160eb486d976
alif_ramadhan <alif@skinmystery.com>
2023-06-28 12:32:22 +0700

    update: dashboard visualisation simple

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # DetectGPT-T5
 Final Project Literature Digital
 
+# Preview Web App
+![](static/preview_app.jpeg)
+
+# Preview Dashboard
+![](static/preview_dashboard.jpeg)
+
 # TO-DO
 - [x] Create Boilerplate Interface
 - [x] Research DetectGPT with T5
 - [x] Integrate UI with SqlLite Database
 - [x] Create Docker Compose .yaml
-- [ ] Create Dashboard Visualisation Related Output Result.
+- [x] Create Dashboard Visualisation Related Output Result.
--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,29 @@
-# TODO: Implement Dashboard Visualisation
-import streamlit as st 
+import streamlit as st
 from utils import read_logs
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
+st.set_page_config(layout="wide")
 
-st.table(read_logs())
+if __name__ == "__main__":
+
+    st.title("Dasboard DetectGPT")
+
+    data = read_logs()
+    col_1, col_2 = st.columns(2)
+
+    with col_1:
+        st.subheader("Score Distribution")
+        temp_df = data[['timestamp', 'score']]
+        temp_df = temp_df.rename(columns = {"timestamp" : "index"}).set_index('index')
+        st.line_chart(temp_df, height = 455)
+
+    with col_2:
+        st.subheader("Text Frequency")
+        list_free_text = " ".join(data['text_input'].tolist())
+        wordcloud = WordCloud(background_color = "white", max_words = 50)
+        wordcloud.generate(list_free_text)
+        fig, ax = plt.subplots()
+        ax.imshow(wordcloud, interpolation = "bilinear")
+        ax.axis("off")
+        st.pyplot(fig)
